ad807b8e4adebc786d25deb793262b488a601bb1
tsubasa_develop <t.ohtsuka@re-tech.net>
2023-07-03 17:22:08 +0900

    feat: Generate git comment based on question type

--- src/routes/api/chat/+server.ts
+++ src/routes/api/chat/+server.ts
@@ -25,7 +25,16 @@ const chat = async ({ content }: { content: string }): Promise<string> => {
 export const POST = (async ({ request }) => {
   const res = await request.json();
   const { question, prefix_comment, prefix_branch, type } = res.params;
-  const max_answer = 3;
+  const max_answer = (() => {
+    switch (type) {
+      case "comment":
+        return Math.max(prefix_comment.length * 3, 3);
+      case "branch":
+        return 3;
+      default:
+        return 3;
+    }
+  })();
   const content = (() => {
     switch (type) {
       case "comment":
@@ -33,10 +42,10 @@ export const POST = (async ({ request }) => {
           `次のような実装を行った。このプロジェクトはgitで管理を行なっており、この変更をcommitしたい。英語でgit commentを考えて。`,
           `接頭辞として ${prefix_comment.join(
             " "
-          )} の中から最も適した接頭辞を選んで。`,
+          )} から順番に3つずつ出力して。(合計${max_answer}個)`,
           `出力形式はgit commentのみで接頭辞から必ず開始すること。接頭辞から開始しない回答は認めない。`,
           `クォーテーションなどで囲みなどは一切不要。`,
-          `回答は${max_answer}種類まで出力し、改行で区切って。接頭辞は適しているかを重視するため、重複しても構わない。`,
+          `回答はそれぞれ\nで区切って。順番は接頭辞毎にグループ化して3つずつ表示して。`,
           `------------------------------`,
           `${question}`,
           `------------------------------`,
@@ -44,27 +53,38 @@ export const POST = (async ({ request }) => {
           `fix: Update image and resize for better viewing`,
           `fix: Replace image and adjust size for optimal display`,
           `fix: Refresh image and resize to improve visibility`,
+          `docs: Update image and resize for better viewing`,
+          `docs: Replace image and adjust size for optimal display`,
+          `docs: Refresh image and resize to improve visibility`,
+          `chore: Update image and resize for better viewing`,
+          `chore: Replace image and adjust size for optimal display`,
+          `chore: Refresh image and resize to improve visibility`,
         ].join("\n");
       case "branch":
-        console.log(prefix_branch, !!prefix_branch);
         return [
           `次のような実装を行う。このプロジェクトはgitで管理を行なっており、作業をする際、新たにブランチを作成する。この作業ブランチの名前を英語で考えて。`,
-          `回答は${max_answer}種類まで出力し、改行で区切って。`,
+          `回答は${max_answer}種類出力し、改行で区切って。`,
           !!prefix_branch ? `接頭辞として"${prefix_branch}"を使用して。` : "",
           `------------------------------`,
           `${question}`,
           `------------------------------`,
           `下記の回答例に従って回答して。`,
-          `update_type_label_format`,
-          `feature/fix_type_label_format`,
-          `docs/update_type_label_format`,
+          `${
+            !!prefix_branch ? prefix_branch + "/" : ""
+          }update_type_label_format`,
+          `${
+            !!prefix_branch ? prefix_branch + "/" : "feature/"
+          }fix_type_label_format`,
+          `${
+            !!prefix_branch ? prefix_branch + "/" : "docs/"
+          }update_type_label_format`,
         ].join("\n");
       default:
         return "";
     }
   })();
   const response = await chat({ content });
-  console.log({ content });
+  console.log({ max_answer, content, response });
   // 改行で区切られていることが期待されるため、配列で返す
   const temp = response.split("\n");
   const answers = new Array(max_answer).fill("").map((_, i) => temp[i] || "");
