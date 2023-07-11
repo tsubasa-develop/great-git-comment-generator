6da4afe9e2e2b112e33c6ee6da5a1c88b76cd18d
tsubasa_develop <t.ohtsuka@re-tech.net>
2023-07-11 11:32:13 +0900

    fix: Update POST function to filter and remove duplicate answers

--- src/routes/api/chat/+server.ts
+++ src/routes/api/chat/+server.ts
@@ -24,7 +24,12 @@ const chat = async ({ content }: { content: string }): Promise<string> => {
 
 export const POST = (async ({ request }) => {
   const res = await request.json();
-  const { question, prefix_comment, prefix_branch, type } = res.params;
+  const { question, prefix_comment, prefix_branch, type } = res.params as {
+    question: string;
+    prefix_comment: string[];
+    prefix_branch: string;
+    type: "comment" | "branch";
+  };
   const max_answer = (() => {
     switch (type) {
       case "comment":
@@ -38,36 +43,34 @@ export const POST = (async ({ request }) => {
   const content = (() => {
     switch (type) {
       case "comment":
+        const example = prefix_comment.flatMap((prefix) => {
+          return new Array(max_answer).fill(
+            `${prefix} Description of implementation content to avoid duplication.`
+          );
+        });
         return [
-          `次のような実装を行った。このプロジェクトはgitで管理を行なっており、この変更をcommitしたい。英語でgit commentを考えて。`,
-          `接頭辞として ${prefix_comment.join(
-            " "
-          )} から順番に3つずつ出力して。(合計${max_answer}個)`,
-          `出力形式はgit commentのみで接頭辞から必ず開始すること。接頭辞から開始しない回答は認めない。`,
-          `クォーテーションなどで囲みなどは一切不要。`,
-          `回答はそれぞれ\nで区切って。順番は接頭辞毎にグループ化して3つずつ表示して。`,
-          `------------------------------`,
-          `${question}`,
-          `------------------------------`,
-          `下記の回答例に従って回答して。`,
-          `fix: Update image and resize for better viewing`,
-          `fix: Replace image and adjust size for optimal display`,
-          `fix: Refresh image and resize to improve visibility`,
-          `docs: Update image and resize for better viewing`,
-          `docs: Replace image and adjust size for optimal display`,
-          `docs: Refresh image and resize to improve visibility`,
-          `chore: Update image and resize for better viewing`,
-          `chore: Replace image and adjust size for optimal display`,
-          `chore: Refresh image and resize to improve visibility`,
+          ...[
+            `次のような実装を行った。このプロジェクトはgitで管理を行なっており、この変更をcommitしたい。英語でgit commentを考えて。`,
+            `接頭辞として ${prefix_comment.join(
+              " "
+            )} から順番に3つずつ出力して。(合計${max_answer}個)`,
+            `出力形式はgit commentのみで接頭辞から必ず開始すること。接頭辞から開始しない回答は認めない。`,
+            `クォーテーションなどで囲みなどは一切不要。`,
+            `回答はそれぞれ\nで区切って。順番は接頭辞毎にグループ化して3つずつ表示して。`,
+            `下記の回答例に従って回答して。`,
+          ],
+          ...example,
+          [
+            `------------------------------`,
+            `${question}`,
+            `------------------------------`,
+          ],
         ].join("\n");
       case "branch":
         return [
           `次のような実装を行う。このプロジェクトはgitで管理を行なっており、作業をする際、新たにブランチを作成する。この作業ブランチの名前を英語で考えて。`,
           `回答は${max_answer}種類出力し、改行で区切って。`,
           !!prefix_branch ? `接頭辞として"${prefix_branch}"を使用して。` : "",
-          `------------------------------`,
-          `${question}`,
-          `------------------------------`,
           `下記の回答例に従って回答して。`,
           `${
             !!prefix_branch ? prefix_branch + "/" : ""
@@ -78,16 +81,36 @@ export const POST = (async ({ request }) => {
           `${
             !!prefix_branch ? prefix_branch + "/" : "docs/"
           }update_type_label_format`,
+          `------------------------------`,
+          `${question}`,
+          `------------------------------`,
         ].join("\n");
       default:
         return "";
     }
   })();
   const response = await chat({ content });
-  console.log({ max_answer, content, response });
   // 改行で区切られていることが期待されるため、配列で返す
   const temp = response.split("\n");
-  const answers = new Array(max_answer).fill("").map((_, i) => temp[i] || "");
+  const answers = new Array(max_answer)
+    .fill("")
+    .map((_, i) => temp[i] || "")
+    .filter((v, _, ary) => {
+      // 重複した内容があれば削除
+      return ary.indexOf(v) === ary.lastIndexOf(v);
+    })
+    .filter((v) => {
+      if (type === "comment") {
+        return prefix_comment.some((prefix) => v.startsWith(prefix));
+      } else {
+        if (!!prefix_branch) {
+          return v.startsWith(prefix_branch);
+        }
+      }
+      return true;
+    })
+    .filter((v) => !!v);
+  console.log({ answers });
   const body = {
     question: question || "",
     answers,
